774b5063eb5951b30cfb6fb8c20085f3c2827659
thbourlove <thbourlove@gmail.com>
2019-01-30 15:18:58 +0800

    check vpay before claim

--- dapppubbosbp.cpp
+++ dapppubbosbp.cpp
@@ -5,17 +5,26 @@ void dapppubbosbp::claim() {
     const auto& prod = _producers.get(_self, "unable to find your producer");
     const uint32_t claim_time = prod.last_claim_time.sec_since_epoch() + sec_per_day + 1;
     if (now() >= claim_time) {
-        action(
-            permission_level{_self, N(active)},
-            N(eosio),
-            N(claimrewards),
-            make_tuple(_self)
-        ).send();
+        auto gstate = _global.get();
+        auto vpay = int64_t((gstate.pervote_bucket * prod.total_votes) / gstate.total_producer_vote_weight);
+        if (vpay < 1000000) {
+            transaction out;
+            out.actions.emplace_back(permission_level{_self, N(active)}, _self, N(claim), std::make_tuple());
+            out.delay_sec = 60 * 10;
+            out.send(N(claim), _self);
+        } else {
+            action(
+                permission_level{_self, N(active)},
+                N(eosio),
+                N(claimrewards),
+                make_tuple(_self)
+            ).send();
 
-        transaction out;
-        out.actions.emplace_back(permission_level{_self, N(active)}, _self, N(claim), std::make_tuple());
-        out.delay_sec = sec_per_day + 1;
-        out.send(N(claim), _self);
+            transaction out;
+            out.actions.emplace_back(permission_level{_self, N(active)}, _self, N(claim), std::make_tuple());
+            out.delay_sec = sec_per_day + 1;
+            out.send(N(claim), _self);
+        }
     } else {
         transaction out;
         out.actions.emplace_back(permission_level{_self, N(active)}, _self, N(claim), std::make_tuple());
--- dapppubbosbp.hpp
+++ dapppubbosbp.hpp
@@ -1,6 +1,8 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/transaction.hpp>
 #include <eosiolib/time.hpp>
+#include <eosiolib/privileged.hpp>
+#include <eosiolib/singleton.hpp>
 
 using namespace eosio;
 using namespace std;
@@ -11,10 +13,39 @@ class dapppubbosbp: public contract {
 public:
     dapppubbosbp(account_name self)
             : contract(self),
-            _producers(N(eosio), N(eosio))
+            _producers(N(eosio), N(eosio)),
+            _global(N(eosio), N(eosio))
         {};
     void claim();
 private:
+    struct eosio_global_state : blockchain_parameters {
+        uint64_t free_ram()const { return max_ram_size - total_ram_bytes_reserved; }
+
+        uint64_t             max_ram_size = 64ll*1024 * 1024 * 1024;
+        uint64_t             total_ram_bytes_reserved = 0;
+        int64_t              total_ram_stake = 0;
+
+        block_timestamp      last_producer_schedule_update;
+        time_point           last_pervote_bucket_fill;
+        int64_t              pervote_bucket = 0;
+        int64_t              perblock_bucket = 0;
+        uint32_t             total_unpaid_blocks = 0; /// all blocks which have been produced but not paid
+        int64_t              total_activated_stake = 0;
+        time_point           thresh_activated_stake_time;
+        uint16_t             last_producer_schedule_size = 0;
+        double               total_producer_vote_weight = 0; /// the sum of all producer votes
+        block_timestamp      last_name_close;
+
+        // explicit serialization macro is not necessary, used here only to improve compilation time
+        EOSLIB_SERIALIZE_DERIVED( eosio_global_state, eosio::blockchain_parameters,
+                                    (max_ram_size)(total_ram_bytes_reserved)(total_ram_stake)
+                                    (last_producer_schedule_update)(last_pervote_bucket_fill)
+                                    (pervote_bucket)(perblock_bucket)(total_unpaid_blocks)(total_activated_stake)(thresh_activated_stake_time)
+                                    (last_producer_schedule_size)(total_producer_vote_weight)(last_name_close) )
+    };
+    typedef eosio::singleton<N(global), eosio_global_state> global_state_singleton;
+    global_state_singleton _global;
+
     struct producer_info {
         account_name          owner;
         double                total_votes = 0;
